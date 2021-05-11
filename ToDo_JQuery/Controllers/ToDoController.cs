1518e556cc831318de2e93264148e87a3710f165
gawronCode <gawroncode@gmail.com>
2021-05-12 00:34:38 +0200

    Add project files.

---
+++ ToDo_JQuery/Controllers/ToDoController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace ToDo_JQuery.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+
+        [HttpGet]
+        public string Get()
+        {
+            return "ToDo";
+        }
+    }
+}
